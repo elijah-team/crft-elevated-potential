91e41229c64dbbfd1faa0180a793ab41aa025b93
Tripleo <noble@tripleo.nonexistent>
2024-08-23 18:59:28 -0400

    Can't publish: jitpack returns nothing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,31 @@
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+
+//    id("com.huanshankeji.maven-publish-conventions") version "0.5.1"
+//    `maven-publish`
 }
 
 rootProject.name = "crft-elevated-potential"
 
 //!!include(":dlog")
 
-include(":tripleo.elijah.stateful-processor")
+//include(":tripleo.elijah.stateful-processor") // !!
 
 include(":annotation-user")
 include(":annotation-processing")
-
 include(":annotations")
 
 // FIXME these depend on elijah apis
 //include(":typeinf")
 //include(":elijah-compiler-gen")
+
+//toolchainManagement {
+//    jvm {
+//        javaRepositories {
+//            repository("foojay") {
+//                resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
+//            }
+//        }
+//    }
+//}
